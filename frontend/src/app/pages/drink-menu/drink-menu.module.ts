db09398a98b16e37b313cb1a72fcf52767fb625a
Gary Vladimir <gary@garybricks.com>
2024-06-21 13:55:08 -0600

    removed entryComponents property

--- frontend/src/app/pages/drink-menu/drink-menu.module.ts
+++ frontend/src/app/pages/drink-menu/drink-menu.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  entryComponents: [DrinkFormComponent],
-  declarations: [DrinkMenuPage, DrinkGraphicComponent, DrinkFormComponent],
+  declarations: [DrinkMenuPage, DrinkFormComponent, DrinkGraphicComponent]
 })
-export class DrinkMenuPageModule {}
+export class DrinkMenuPageModule { }
+
